27b1c5cbc7ea008eea8945cfb990f4393032de23
natapond <63299508+natapond@users.noreply.github.com>
2020-04-07 20:27:39 +0700

    Update DS02_LightSensorToLine.ino

--- esp32/DS02_LightSensorToLine.ino
+++ esp32/DS02_LightSensorToLine.ino
@@ -4,8 +4,8 @@
    modified by Digital Siam
 */
 #include <TridentTD_LineNotify.h>
-#define SSID        "WIFI"
-#define PASSWORD    "PASSWORD"
+#define SSID        "ABCD"
+#define PASSWORD    "18101999"
 #define LINE_TOKEN  "TOKEN"
 
 void setup(){
